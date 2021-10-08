a9795a2b6942f3b1629c91e25fa44d614d1d0d50
Sergi <sergisise@gmail.com>
2021-10-08 11:30:11 +0200

    Se ha movido de carpeta conexion.php

--- LogicaNegocio/guardarMedicion.php
+++ LogicaNegocio/guardarMedicion.php
@@ -3,7 +3,7 @@
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){//comprobamos que tipo de peticion REST es
     
-    require_once 'conexion.php';//accedemos a la conexion de la bbdd
+    require_once '../conexion.php';//accedemos a la conexion de la bbdd
     
     //obtenemos la info
     $data = file_get_contents('php://input');
