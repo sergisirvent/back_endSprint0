f4679a887d59c251940382ec409047f2d6149a5c
Sergi <sergisise@gmail.com>
2021-10-18 19:18:00 +0200

    falta comentar

---
+++ src/ux/LogicaFake/obtenerUltimasMediciones.js
@@ -0,0 +1,26 @@
+function obtenerUltimasMediciones(cb,cuantas){
+    // preparar la llamada remota
+    var xmlhttp = new XMLHttpRequest()
+    xmlhttp.onreadystatechange = function() {
+        // callback para cuando llegue la respuesta
+        // de la petición que haremos más abajo
+
+        if( this.readyState == 4 && this.status == 200 ){
+            // este es el texto JSON recibido la llamada a
+            // demo_file.php, pasado a objeto JSON
+            console.log( "recibo: " + this.responseText )
+            var resultado = this.responseText;
+
+            console.log("Este es el resultado: -->>"+resultado);
+
+
+            // no hay error, devuelvo el resultado
+            cb( null, resultado )
+        }
+    }
+
+    // llamamos *remotamente* al fichero diHola.php
+    // (la verdadera función de la lógica)
+    xmlhttp.open("GET", "../LogicaNegocio/obtenerUltimasMediciones.php?Cuantas="+cuantas, true)
+    xmlhttp.send()
+}
